d8816b54987024389779d3aab8da05e9d5798321
Beoveo <beavillegas1997@gmail.com>
2019-03-18 00:42:15 +0100

    Cambios, pantalla 1 de creacion hecha
    Falta la pantalla 2. Y acabar la barra izquierda

--- P2/creacion.php
+++ P2/creacion.php
@@ -2,11 +2,12 @@
 session_start();
 ?>
 
+<!-- Segunda pantalla de creacion -->
+
 <!DOCTYPE html>
 <html>
 <head>
 	<link rel="stylesheet" type="text/css" href="estilo.css" />
-	<link rel="stylesheet" type="text/css" href="forms.css" />
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<title>Creación</title>
 </head>
@@ -21,25 +22,19 @@ session_start();
 
 				<div id="contenidocreacion">
 
-					<div id="eligemapa">
+					<div id="pantalla">
+						<img src="img/fondo.png" alt="">
+					</div>
+
+					<div id="pregunta">
 						<h1> Elige un mapa </h1>
 						<p>Cosas sobre creacioooooooooon por cosd sdfsfi dfe sa fdwseeg sdfsefs srf. </p>
 					</div>
 
-					<div id="eligemonstruo">
+					<div id="respuesta">
 						<h1> Elige un monstruo </h1>
 						<p>Cosas sobre creacioooooooooon por cosd sdfsfi dfe sa fdwseeg sdfsefs srf. </p>
 					</div>
-
-					<div id="eligeobjeto">
-						<h1> Elige un objeto </h1>
-						<p>Cosas sobre creacioooooooooon por cosd sdfsfi dfe sa fdwseeg sdfsefs srf. </p>
-					</div>
-					<div id="contenidoboton">
-					<form action="index.php" method="POST">
-  						<p><button type="submit">CREAR</button></p>
-					</form>
-				</div>
 				</div>
 
 				<?php 
---
+++ P2/seleccionCreacion.php
@@ -0,0 +1,51 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="estilo.css" />
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>Selección Creación</title>
+</head>
+
+<body>
+	<div id="contenedor">
+
+		<?php
+		include('cabecera.php');
+				include('sidebarIzq.php'); //Aqui va el grid
+				?>
+
+				<div id="contenidocreacion">
+
+					<div id="eligemapa">
+						<h1> Elige un mapa </h1>
+						<p>Cosas sobre creacioooooooooon por cosd sdfsfi dfe sa fdwseeg sdfsefs srf. </p>
+					</div>
+
+					<div id="eligemonstruo">
+						<h1> Elige un monstruo </h1>
+						<p>Cosas sobre creacioooooooooon por cosd sdfsfi dfe sa fdwseeg sdfsefs srf. </p>
+					</div>
+
+					<div id="eligeobjeto">
+						<h1> Elige un objeto </h1>
+						<p>Cosas sobre creacioooooooooon por cosd sdfsfi dfe sa fdwseeg sdfsefs srf. </p>
+					</div>
+					<div id="contenidoboton">
+					<form action="creacion.php" method="POST">
+  						<p><button type="submit">CREAR</button></p>
+					</form>
+				</div>
+				</div>
+
+				<?php 
+					include("pie.php");
+				?>
+
+			</div> <!-- Fin del contenedor -->
+
+		</body>
+		</html>
