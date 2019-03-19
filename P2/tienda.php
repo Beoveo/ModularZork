4e304dba1d00645f57b9ce47e4a85c60fa6fab34
lbarja5 <lbarja@ucm.es>
2019-03-19 01:16:04 +0100

    update tienda + style

--- P2/tienda.php
+++ P2/tienda.php
@@ -8,7 +8,7 @@
 		<link rel="stylesheet" type="text/css" href="estilo.css" />
 		<link rel="stylesheet" type="text/css" href="forms.css" />
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-		<title>Portada</title>
+		<title>Tienda</title>
 	</head>
 
 	<body>
@@ -20,14 +20,132 @@
 			?>
 
 			<div id="contenido">
+				<div id="ofertas">
+					<h1>Ofertas!</h1>
+					<ul class=listObj id="listOfertas">
+						<li class="item">
+							<div class="imgen">
+								<img class="imgObj" src="ObjEjem/knive.png"/>
+							</div>
+							<p>Cuchillo</p>
+							<p>10 Zorkies</p>
+						</li>
+						<li class="item">
+							<div class="imgen">
+								<img class="imgObj" src="ObjEjem/axe.png"/>
+							</div>
+							<p>Hacha</p>
+							<p>50 Zorkies</p>
+						</li>
+						<li class="item">
+							<div class="imgen">
+								<img class="imgObj" src="ObjEjem/sword.png"/>
+							</div>
+							<p>Espada</p>
+							<p>100 Zorkies</p>
+						</li>
+						<li class="item">
+							<div class="imgen">
+								
+							</div>
+							<p></p>
+							<p></p>
+						</li>
+						<li class="item">
+							<div class="imgen">
+								
+							</div>
+							<p></p>
+							<p></p>
+						</li>
+					</ul>
+				</div>
 				<div id="mapas">
 					<h1>Mapas</h1>
+					<ul class=listObj id="listaMapas">
+						<li class="item">
+							<div class="imgen">
+								
+							</div>
+							<p></p>
+						</li>
+						<li class="item">
+							<div class="imgen">
+								
+							</div>
+							<p></p>
+						</li>
+						<li class="item">
+							<div class="imgen">
+								
+							</div>
+							<p></p>
+						</li>
+						<li class="item">
+							<div class="imgen">
+								
+							</div>
+							<p></p>
+						</li>
+					</ul>
 				</div>
 				<div id="personajes">
 					<h1>Personajes</h1>
+					<ul class=listObj id="listaPersonajes">
+						<li class="item">
+							<div class="imgen">
+								
+							</div>
+							<p></p>
+						</li>
+						<li class="item">
+							<div class="imgen">
+								
+							</div>
+							<p></p>
+						</li>
+						<li class="item">
+							<div class="imgen">
+								
+							</div>
+							<p></p>
+						</li>
+						<li class="item">
+							<div class="imgen">
+								
+							</div>
+							<p></p>
+						</li>
+					</ul>
 				</div>
 				<div id="objetos">
 					<h1>Objetos</h1>
+					<ul class=listObj id="listaObjetos">
+						<li class="item">
+							<div class="imgen">
+								
+							</div>
+							<p></p>
+						</li>
+						<li class="item">
+							<div class="imgen">
+								
+							</div>
+							<p></p>
+						</li>
+						<li class="item">
+							<div class="imgen">
+								
+							</div>
+							<p></p>
+						</li>
+						<li class="item">
+							<div class="imgen">
+								
+							</div>
+							<p></p>
+						</li>
+					</ul>
 				</div>
 			</div>
 
